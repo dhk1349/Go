812c4632c70c9fa19441aeee66338bbe4a6843f4
dhk1349 <41980618+dhk1349@users.noreply.github.com>
2020-03-18 22:09:59 +0900

    .

---
+++ 1. print/main.go
@@ -0,0 +1,14 @@
+package main
+
+import (
+	"fmt"
+	"user/dhk1349/learngo/something"
+)
+
+func main() {
+	fmt.Println("hello")
+	something.SayHello()
+	something.SayBye()
+
+	const name string = "dhk1349"
+}
